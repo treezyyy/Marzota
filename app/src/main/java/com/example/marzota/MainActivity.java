5343b43346110227a54411b91c9feaa99e5c5716
marklum7 <122746275+marklum7@users.noreply.github.com>
2023-04-15 01:42:36 +0300

    marzota

--- app/src/main/java/com/example/marzota/MainActivity.java
+++ app/src/main/java/com/example/marzota/MainActivity.java
@@ -1,14 +1,36 @@
-package com.example.marzota;
+package com.example.marzota.;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] marzota = {"Marklum", "G2N", "керыч", "flipside", "l3koyn", "demit", "buyan", "maloyi","Mcпох","kirya","fontazer"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, marzota);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, Result.class);
+                intent.putExtra("cartoon", marzota[position]);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/marzota/Result.java
+++ app/src/main/java/com/example/marzota/Result.java
@@ -1,14 +1,45 @@
-package com.example.marzota;
+package com.example.marzota.;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class Result extends AppCompatActivity {
+
+    LinearLayout LinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        LinearLayout = findViewById(R.id.text_view);
+        Intent intent = getIntent();
+        String cartoon = intent.getStringExtra("marzota");
+
+        if (cartoon.equals("Marklum")) {
+            LinearLayout.setText();
+        } else if (cartoon.equals("G2N")) {
+            LinearLayout.setText();
+        } else if (cartoon.equals("керыч")) {
+            LinearLayout.setText();
+        } else if (cartoon.equals("flipside")) {
+            LinearLayout.setText();
+        } else if (cartoon.equals("l3koyn")) {
+            LinearLayout.setText();
+        } else if (cartoon.equals("demit")) {
+            LinearLayout.setText();
+        } else if (cartoon.equals("buyan")) {
+            LinearLayout.setText();
+        } else if (cartoon.equals("maloyi")) {
+            LinearLayout.setText();
+        } else if (cartoon.equals("Mcпох")) {
+            LinearLayout.setText();
+        } else if (cartoon.equals("kirya")) {
+            LinearLayout.setText();
+        } else if (cartoon.equals("fontazer")) {
+            LinearLayout.setText();
+        }
     }
 }
