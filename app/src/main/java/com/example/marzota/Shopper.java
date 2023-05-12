c4b20266fe7bd303ec578088d3b967f6ad113f27
treezyyy <122749977+treezyyy@users.noreply.github.com>
2023-05-12 23:51:51 +0300

    MANY

---
+++ app/src/main/java/com/example/marzota/Shopper.java
@@ -0,0 +1,14 @@
+package com.example.marzota;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shopper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopper);
+    }
+}
