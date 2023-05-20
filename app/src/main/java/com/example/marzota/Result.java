f8f2664cb4a268f20d725be53961f67e2e391aee
marklum7 <vapilin777@gmail.com>
2023-05-20 16:38:42 +0300

    финиш

--- app/src/main/java/com/example/marzota/Result.java
+++ app/src/main/java/com/example/marzota/Result.java
@@ -4,20 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.res.ResourcesCompat;
-
-import android.content.Intent;
-import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
-import java.net.URI;
+
 
 public class Result extends AppCompatActivity {
 
@@ -29,5 +25,46 @@ public class Result extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        ImageView tg;
+        tg = (ImageView) findViewById(R.id.tg);
+        tg.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent browserIntent = new
+                        Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/Marzota"));
+                startActivity(browserIntent);
+            }
+        });
+        ImageView ds;
+        ds = (ImageView) findViewById(R.id.ds);
+        ds.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent browserIntent = new
+                        Intent(Intent.ACTION_VIEW, Uri.parse("https://discord.gg/r4ckTgNZRh"));
+                startActivity(browserIntent);
+            }
+        });
+        ImageView tv;
+        tv = (ImageView) findViewById(R.id.tv);
+        tv.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent browserIntent = new
+                        Intent(Intent.ACTION_VIEW, Uri.parse("https://www.twitch.tv/marklum_7"));
+                startActivity(browserIntent);
+            }
+        });
+        ImageView vk;
+        vk = (ImageView) findViewById(R.id.vk);
+        vk.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent browserIntent = new
+                        Intent(Intent.ACTION_VIEW, Uri.parse("https://vk.com/fliips1de"));
+                startActivity(browserIntent);
+            }
+        });
     }
+
 }
